e1f5ba580d2703ba7319e2e5156e6472f3034d17
gemma <gemma@DESKTOP-2Q2G917>
2022-11-16 10:33:25 +0000

    Add project files.

---
+++ Selection - Task 2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Selection___Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string q1;
+            string q2;
+            string q3;
+
+            Console.WriteLine("Is the lamp unplugged?");
+            q1 = Console.ReadLine();
+            if (q1 == "yes")
+            {
+                Console.WriteLine("Plug in the lamp");
+            }
+
+            else
+            {
+                Console.WriteLine("Is the bulb burned out?");
+            }
+
+            q2 = Console.ReadLine();
+            if (q2 == "yes")
+            {
+                Console.WriteLine("Replace the bulb");
+            }
+
+            else
+            {
+                Console.WriteLine("Is the fuse blown?");
+            }
+
+            q3 = Console.ReadLine();
+            if (q3 == "yes")
+            {
+                Console.WriteLine("Replace furnace");
+            }
+
+            else
+            {
+                Console.WriteLine("Buy a new lamp");
+            }
+        }
+    }
+}
